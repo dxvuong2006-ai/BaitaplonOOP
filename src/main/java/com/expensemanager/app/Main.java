8c64952e4861fcd8adf84fa8543c8cdc595f7d19
Lê Phương Thúy <24022743@vnu.edu.vn>
2026-07-25 11:57:24 +0700

    Add ConsoleView: hien thi menu va dieu phoi lua chon nguoi dung

--- src/main/java/com/expensemanager/app/Main.java
+++ src/main/java/com/expensemanager/app/Main.java
@@ -1,4 +1,14 @@
 package com.expensemanager.app;
 
+import com.expensemanager.view.ConsoleView;
+
+/**
+ * Điểm khởi đầu (entry point) của toàn bộ ứng dụng.
+ * Đây là nơi duy nhất chương trình Java bắt đầu chạy,
+ * có nhiệm vụ khởi tạo và gọi tầng View để hiển thị giao diện.
+ */
 public class Main {
+    public static void main(String[] args) {
+        new ConsoleView().start();
+    }
 }
--- src/test/java/com/expensemanager/model/model.java
+++ src/test/java/com/expensemanager/model/model.java
@@ -1,4 +1,3 @@
 package com.expensemanager.model;
-
 public class model {
 }
